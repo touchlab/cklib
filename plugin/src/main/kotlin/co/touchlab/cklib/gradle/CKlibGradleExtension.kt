4c9a2e58e7d35e319e30a024cbeb2ff27359b434
Kevin Galligan <kgalligan@gmail.com>
2021-10-09 15:55:59 -0400

    Add paths for linux

--- plugin/src/main/kotlin/co/touchlab/cklib/gradle/CKlibGradleExtension.kt
+++ plugin/src/main/kotlin/co/touchlab/cklib/gradle/CKlibGradleExtension.kt
@@ -13,11 +13,13 @@ package co.touchlab.cklib.gradle
 import co.touchlab.cklib.gradle.reflection.PlatformManager
 import org.gradle.api.Project
 import org.jetbrains.kotlin.konan.target.Distribution
+import org.jetbrains.kotlin.konan.target.HostManager
+import org.jetbrains.kotlin.konan.target.TargetSupportException
 import javax.inject.Inject
 
 open class CKlibGradleExtension @Inject constructor(val project: Project){
-    var konanHome: String = "${System.getProperty("user.home")}/.konan/kotlin-native-prebuilt-macos-x86_64-${GradleValues.KOTLIN_VERSION}"
-    var llvmHome: String = "${System.getProperty("user.home")}/.konan/dependencies/clang-llvm-apple-8.0.0-darwin-macos"
+    var konanHome: String = "${System.getProperty("user.home")}/.konan/kotlin-native-prebuilt-${simpleOsName}-x86_64-${GradleValues.KOTLIN_VERSION}"
+    var llvmHome: String = "${System.getProperty("user.home")}/.konan/dependencies/${llvmName}"
 }
 
 internal val Project.platformManager: PlatformManager
@@ -28,3 +30,35 @@ internal val Project.platformManager: PlatformManager
 
 internal val Project.llvmHome: String
     get() = extensions.getByType(CKlibGradleExtension::class.java).llvmHome
+
+internal val simpleOsName: String
+    get() {
+        val hostOs = HostManager.hostOs()
+        return if (hostOs == "osx") "macos" else hostOs
+    }
+
+internal val osName: String
+    get() {
+        val javaOsName = System.getProperty("os.name")
+        return when {
+            javaOsName == "Mac OS X" -> "osx"
+            javaOsName == "Linux" -> "linux"
+            javaOsName.startsWith("Windows") -> "windows"
+            else -> throw TargetSupportException("Unknown operating system: $javaOsName")
+        }
+    }
+
+internal val llvmName: String
+    get() {
+        return when (osName) {
+            "osx" -> llvm_macos_x64
+            "linux" -> llvm_linux_x64
+            "windows" -> llvm_mingw_x64
+            else -> throw TargetSupportException("Unknown operating system: $osName")
+        }
+    }
+
+internal val llvm_linux_x64 = "clang-llvm-8.0.0-linux-x86-64"
+internal val llvm_mingw_x64 = "msys2-mingw-w64-x86_64-clang-llvm-lld-compiler_rt-8.0.1"
+internal val llvm_macos_x64 = "clang-llvm-apple-8.0.0-darwin-macos"
+internal val llvm_macos_arm64 = "clang-llvm-apple-8.0.0-darwin-macos-aarch64"
