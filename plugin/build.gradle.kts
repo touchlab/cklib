20d76db43c8cdf4016b36463b29c79e0138cd513
Kevin Galligan <kgalligan@gmail.com>
2023-11-01 20:33:07 -0400

    Clean up commented code

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -38,11 +38,3 @@ gradlePlugin {
 repositories {
   mavenCentral()
 }
-//val compileKotlin: KotlinCompile by tasks
-//compileKotlin.kotlinOptions {
-//  jvmTarget = "1.8"
-//}
-//val compileTestKotlin: KotlinCompile by tasks
-//compileTestKotlin.kotlinOptions {
-//  jvmTarget = "1.8"
-//}
